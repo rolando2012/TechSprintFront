c44554a79ec53efff79353214d8e944f00937075
rolando2012 <rolamdomr@gmail.com>
2025-04-19 18:27:20 -0400

    Refactorizar el diseño y el estilo: actualizar la configuración de fuentes, ajustar el esquema de colores y simplificar el componente Inicio

---
+++ src/config/fonts.ts
@@ -0,0 +1,9 @@
+import { ADLaM_Display } from 'next/font/google'
+
+
+export const adlam = ADLaM_Display({
+    subsets: ['latin', 'adlam'],    // ADLaM es otro subconjunto aparte de latin :contentReference[oaicite:1]{index=1}
+    weight: ['400'],
+    display: 'swap',
+    variable: '--font-adlam',
+});
