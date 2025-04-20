bc61d6e8a8a0864534be29d7e7ecc159394caa21
rolando2012 <rolamdomr@gmail.com>
2025-04-20 05:32:09 -0400

    Agregar estilos de botón y ajustar el componente Inicio: definir colores de botón y simplificar la estructura del componente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,9 @@ import "./globals.css";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 ">
-      <main className={`flex flex-col gap-[32px] row-start-2 items-center sm:items-start text-bright-gray-950 ${adlam.className}`}>
-      <h1 >inicio</h1>
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <button className={`bg-boton hover:bg-boton-hover text-white ${adlam.className} py-2 px-4 rounded-full`}>
+        Registrarse</button>
       </main>
     </div>
   );
